b12bef884fac98ff9eb6b1dad0933d18c2ba7e14
Ismail Guenay <guenay.ismail@googlemail.com>
2026-03-06 20:35:34 +0100

    refactor: user input of day added

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -27,7 +27,7 @@ def get_filters():
     month = input("Specify a month (all, january, february, march, april, may, june): ")
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    day = input("Specify a day of week (all, monday, tuesday, wednesday, thursday, friday, saturday, sunday): ")
 
     print('-'*40)
     return city, month, day
